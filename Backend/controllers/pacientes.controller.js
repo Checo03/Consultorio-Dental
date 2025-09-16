f7022050974abd5851a8e9b27085f45374c333a0
unknown <diegodiegopa83@gmail.com>
2025-09-16 02:41:53 -0600

    Conexion con la BD

---
+++ Backend/controllers/pacientes.controller.js
@@ -0,0 +1,3 @@
+export const getPacientes = (req, res) => {
+    res.send('Lista de pacientes');
+}
---
+++ Backend/database/connection.js
@@ -0,0 +1,24 @@
+import sql from "mssql";
+
+const dbSettings = {
+    user: "Dev",
+    password: "123456",
+    server: "localhost",
+    database: "SistemaDental",
+    options: {
+        encrypt : true,
+        trustServerCertificate: true,
+    },
+};
+
+async function getConnection() {
+    
+    try {
+        const pool = await sql.connect(dbSettings)
+        return pool;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getConnection();
